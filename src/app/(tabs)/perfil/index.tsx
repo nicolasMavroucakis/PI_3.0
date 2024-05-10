dff1810713077e08bc076e04abb83802bbdf2145
Xavier <xavier.gabrielf@gmail.com>
2024-05-10 17:53:40 -0300

    Inicio Tela Perfil

---
+++ src/app/(tabs)/perfil/index.tsx
@@ -0,0 +1,32 @@
+import {Image, Text, View, TextInput, Button, Dimensions} from "react-native";
+import { Link } from 'expo-router';
+import stylePerfil from "../../styles/stylePerfil";
+import auth from 'firebase/database';
+import StartFirebase from '../../../crud/firebaseConfig';
+import { useState } from 'react';
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window')
+
+export default function Perfil(){
+    return (
+        <View>
+            <View style={stylePerfil.TitleImg}>
+            <Text style={stylePerfil.title}>Perfil</Text>
+            <Image source={require('../../../assets/logo.png')} />
+          </View>
+          <View>
+            <TextInput placeholder="Nome" style={[stylePerfil.InputStyle, { width:screenWidth * 0.75  }]} />
+            <TextInput placeholder="E-mail" textContentType="emailAddress" style={[stylePerfil.InputStyle, { width:screenWidth * 0.75 }]} />
+            <TextInput placeholder="Nascimento" textContentType="birthdateYear" style={[stylePerfil.InputStyle, { width:screenWidth * 0.75 }]} />
+            <TextInput placeholder="Senha" textContentType="password" style={[stylePerfil.InputStyle, { width:screenWidth * 0.75 }]}/>
+            <TextInput placeholder="Altura" style={[stylePerfil.InputStyle, { width:screenWidth * 0.75 }]} />
+            <TextInput placeholder="Peso" style={[stylePerfil.InputStyle, { width:screenWidth * 0.75 }]} />
+          </View>
+          <View style={[stylePerfil.ButtonViewStyle, { width:screenWidth * 0.75 }]}>
+            <Link href={"../(tabs)"} asChild style={{width:"100%", height:"100%"}}>
+              <Button title="Cadastrar" color={'white'} />
+            </Link>
+          </View>
+        </View>
+    )
+}
---
+++ src/app/styles/stylePerfil.tsx
@@ -0,0 +1,67 @@
+import {StyleSheet, Dimensions} from "react-native";
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window')
+
+const stylePerfil = StyleSheet.create({
+    TelaDisplay: {
+        justifyContent:'space-between',
+        textAlign:'center',
+        alignItems:'center',
+        display:'flex',
+        
+    },
+    title: {
+        fontSize:20,
+        fontWeight: '700'
+    },
+    TitleImg:{
+        justifyContent:'center',
+        textAlign:'center',
+        alignItems:'center',
+        marginTop:20
+    },
+    InputStyle: {
+        height:60,
+        width:300,
+        borderBottomColor:'black',
+        borderBottomWidth:3,
+        marginTop:10,
+        fontSize:20
+    },
+    ButtonViewStyle:{
+        marginTop:35,
+        justifyContent:'center',
+        alignItems:'center',
+        width:300,
+        height:60,
+        color:'white',
+        backgroundColor:'#A1D5B0',
+        borderRadius:10
+    },   
+    ButtonLinkStyle:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center', 
+        marginTop:10
+    },
+    Hr: {
+        borderColor:'black',
+        borderBottomWidth:2
+    },
+    ButtonGoogle: {
+        backgroundColor:'#E1DFDF'
+    },
+    Img: {
+        position:'relative',
+        top:10,
+        marginRight:6 
+    },
+    DisplaFlex:{
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        flexDirection:'row'
+    }
+})
+
+export default stylePerfil
