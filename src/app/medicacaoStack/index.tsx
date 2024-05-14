edfb42c062d3b091cba67f9d50a101665125127d
NICOLAS DE ALMEIDA MAVROUCAKIS <23.01274-9@maua.br>
2024-05-14 09:23:51 -0300

    estilo da tela de mediacemnto criado

--- src/app/medicacaoStack/index.tsx
+++ src/app/medicacaoStack/index.tsx
@@ -1,11 +1,59 @@
-import { Text, View, TextInput } from "react-native"
+import React, { useState } from "react"
+import { Text, View, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView, Button } from "react-native"
+import styleMedicacaoAdiciona from "../styles/styleMedicacaoAdiciona"
+import { Link } from "expo-router";
 
 export default function medicacaoAdd () {
+    const [onOff, setOnOff] = useState(true);
+
+    const toggleAlarm = () => {
+        setOnOff(!onOff); // Alterna entre ligado e desligado
+    }
+
     return (
-        <View>
-            <Text>
-                aaaaaaasdasdad
-            </Text>
-        </View>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+            <ScrollView contentContainerStyle={{ flexGrow: 1, display: 'flex', justifyContent:'center'  }}>
+                <View style={styleMedicacaoAdiciona.center}>
+                    <Text style={{fontSize:40, paddingBottom: 10}}>Adicionar Medicamento</Text>
+                    <View>
+                        <Text style={{fontSize:20}}>Medicação:</Text>
+                        <TextInput style={styleMedicacaoAdiciona.input}/>
+                    </View>
+                    <View>
+                        <Text style={{fontSize:20}}>Quantidade</Text>
+                        <TextInput style={styleMedicacaoAdiciona.input}/> 
+                    </View>
+                    <View>
+                        <Text style={{fontSize:20}}>Hora:</Text>
+                        <TextInput style={styleMedicacaoAdiciona.input}/> 
+                    </View>
+                    <View>
+                        <Text style={{fontSize:20}}>Descrição:</Text>
+                        <TextInput style={styleMedicacaoAdiciona.input}/> 
+                    </View>
+                    <View style={styleMedicacaoAdiciona.esquerda}>
+                        <Text style={{fontSize:20}}>Alarme</Text>
+                        <TouchableOpacity style={styleMedicacaoAdiciona.alarme} onPress={toggleAlarm}>
+                            {onOff ? (
+                                <>
+                                    <View style={styleMedicacaoAdiciona.alarmeOn}/>
+                                    <Text style={{marginRight:10}}>ON</Text>
+                                </>
+                            ) : (
+                                <>
+                                    <Text style={{marginLeft:5}}>OFF</Text>
+                                    <View style={styleMedicacaoAdiciona.alarmeOff}/>
+                                </>
+                            )}
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styleMedicacaoAdiciona.ButtonViewStyle}>
+                        <Link href={'./Login'} asChild>
+                            <Button title="Adicionar" color={'black'} />
+                        </Link>
+                    </View>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
---
+++ src/app/styles/styleMedicacaoAdiciona.tsx
@@ -0,0 +1,61 @@
+import {StyleSheet, Dimensions} from "react-native";
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window')
+
+const styleMedicacaoAdiciona = StyleSheet.create({
+    input: {
+        width: screenWidth * 0.75,
+        height: 50,
+        borderRadius: 20,
+        backgroundColor: '#bdbdbd',
+        paddingLeft: 5
+    },
+    center: {
+        display: "flex",
+        alignItems: "center",
+        paddingTop: screenHeight * 0.1,
+        height: screenHeight,
+        gap: 10
+    },
+    esquerda: {
+        display: "flex",
+        alignItems: "flex-start",
+        width: screenWidth * 0.75
+    },
+    alarme: {
+        width: screenWidth * 0.2,
+        height:40,
+        backgroundColor: '#bdbdbd',
+        borderRadius: 20,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent:"space-between"
+    },
+    alarmeOn: {
+        height: 30,
+        width: 40,
+        marginLeft: 5,
+        borderRadius: 50,
+        backgroundColor: "green"
+    },
+    alarmeOff: {
+        height: 30,
+        width: 40,
+        marginRight: 5,
+        borderRadius: 50,
+        backgroundColor: "red" 
+    },
+    ButtonViewStyle:{
+        marginTop:10,
+        justifyContent:'center',
+        alignItems:'center',
+        width:300,
+        height:60,
+        color:'white',
+        backgroundColor:'#A1D5B0',
+        borderRadius:10
+    },  
+})
+
+export default styleMedicacaoAdiciona
