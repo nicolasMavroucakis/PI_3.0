6105fda4c65b721cd576b87a5e86a35a566ccb62
Henrique Lin Ye <133709145+HenriqueLinYe@users.noreply.github.com>
2024-05-15 21:58:02 -0300

    Update index.tsx

--- src/app/(tabs)/consultas/index.tsx
+++ src/app/(tabs)/consultas/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Modal, TextInput, Button, } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, TextInput, Button, Switch } from 'react-native';
 import { Image } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import stylesConsulta from '../../styles/stylesConsulta';
 import { useNavigation } from '@react-navigation/native';
 import { Link } from 'expo-router';
+import {ThemeProvider} from 'styled-components';
 
-export default function Consultas() {
+export default function Consultas({ modoEscuro, toggleModoEscuro }) {
     const [modalVisible, setModalVisible] = useState(false);
     const [consultas, setConsultas] = useState([]);
     const [consultaInfo, setConsultaInfo] = useState({
@@ -19,6 +20,15 @@ export default function Consultas() {
 
     const [excluirModalVisible, setExcluirModalVisible] = useState(false);
     const [consultaParaExcluir, setConsultaParaExcluir] = useState(null);
+
+
+    const tema = modoEscuro ? {
+        background: '#000000',
+        text: '#FFFFFF'
+    } : {
+        background: '#FFFFFF',
+        text: '#000000'
+    };
 
     const handleSalvarConsulta = () => {
         const novaConsulta = {
@@ -55,109 +65,117 @@ export default function Consultas() {
     const handleVoltar = () => {
       navigation.goBack();
     };
-    
-    return (
-        <View style={{ flex: 1 }}>
-            <View style={stylesConsulta.divUsuario}>
-                <View style={stylesConsulta.containerUsuario}>
-                    <Image source={require('../../../../assets/user.png')} style={stylesConsulta.imgUsuario} />
-                    <Link href={"../../perfil"} style={stylesConsulta.nomeUsuario}>Nome do Usuário</Link>
-                </View>
-            </View>
-            <View style={stylesConsulta.container}>
-                <Text style={stylesConsulta.title}>
-                    Consultas
-                </Text>
-                <TouchableOpacity style={stylesConsulta.addButton} onPress={() => setModalVisible(true)}>
-                    <AntDesign name="plus" size={28} color="white" />
-                </TouchableOpacity>
-            </View>
 
-            <View style={stylesConsulta.consultaList}>
-                {consultas.map((consulta, index) => (
-                    <View key={index} style={stylesConsulta.consultaItem}>
-                        <View style={stylesConsulta.consultaExibition}>
-                            <Text style={stylesConsulta.consultaTitle}>{consulta.nome}</Text>
-                            <TouchableOpacity onPress={toggleDetalhesConsulta}>
-                                <AntDesign name={mostrarDetalhes ? "down" : "right"} size={28} color="black" />
+    return (
+        <ThemeProvider theme={stylesConsulta}>
+            <View style={{ flex: 1, backgroundColor: modoEscuro ? '#000000' : '#FFFFFF' }}>
+                <View style={stylesConsulta.divUsuario}>
+                    <View style={stylesConsulta.containerUsuario}>
+                        <Image source={require('../../../../assets/user.png')} style={stylesConsulta.imgUsuario} />
+                        <Link href={"../../perfil"} style={stylesConsulta.nomeUsuario}>Nome do Usuário</Link>
+                    </View>
+                </View>
+                <View style={stylesConsulta.container}>
+                    <Switch
+                        value={modoEscuro}
+                        onValueChange={toggleModoEscuro}
+                        thumbColor="#A1D5B0"
+                        trackColor={{ false: "#CCCCCC", true: "#A1D5B0" }}
+                    />
+                    <Text style={[stylesConsulta.title,  modoEscuro && stylesConsulta.titleDarkMode]}>
+                        Consultas
+                    </Text>
+                    <TouchableOpacity style={stylesConsulta.addButton} onPress={() => setModalVisible(true)}>
+                        <AntDesign name="plus" size={28} color="white" />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={stylesConsulta.consultaList}>
+                    {consultas.map((consulta, index) => (
+                        <View key={index} style={stylesConsulta.consultaItem}>
+                            <View style={stylesConsulta.consultaExibition}>
+                                <Text style={stylesConsulta.consultaTitle}>{consulta.nome}</Text>
+                                <TouchableOpacity onPress={toggleDetalhesConsulta}>
+                                    <AntDesign name={mostrarDetalhes ? "down" : "right"} size={28} color="black" />
+                                </TouchableOpacity>
+                            </View>
+                        {mostrarDetalhes && (
+                            <View style={stylesConsulta.detalhesConsulta}>
+                                <Text style={stylesConsulta.detalheText}>Data: {consulta.data}</Text>
+                                <Text style={stylesConsulta.detalheText}>Horário: {consulta.horario}</Text>
+                                <View>
+                                    <TouchableOpacity onPress={() => handleExcluirConsulta(index)}>
+                                        <Image style={stylesConsulta.LixoImg} source={require('../../../../assets/excluir.png')}/>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        )}
+                        </View>
+                    
+                    ))}
+                </View>
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={stylesConsulta.modalBackground}>
+                        <View style={stylesConsulta.modalContent}>
+                            <Text style={stylesConsulta.label}>Nome da Consulta</Text>
+                            <TextInput
+                                style={stylesConsulta.input}
+                                placeholder="Digite o nome da consulta"
+                                onChangeText={(text) => setConsultaInfo({ ...consultaInfo, nome: text })}
+                            />
+                            <Text style={stylesConsulta.label}>Data</Text>
+                            <TextInput
+                                style={stylesConsulta.input}
+                                placeholder="Digite a data da consulta"
+                                onChangeText={(text) => setConsultaInfo({ ...consultaInfo, data: text })}
+                            />
+                            <Text style={stylesConsulta.label}>Horário</Text>
+                            <TextInput
+                                style={stylesConsulta.input}
+                                placeholder="Digite o horário da consulta"
+                                onChangeText={(text) => setConsultaInfo({ ...consultaInfo, horario: text })}
+                            />
+                            <TouchableOpacity onPress={handleSalvarConsulta} style={[stylesConsulta.botaoSalvar, { backgroundColor: '#A1D5B0' }]}>
+                                <Text style={{ color: 'white', textAlign: 'center', fontSize: 16, }}>Salvar Consulta</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={stylesConsulta.arrowButton} onPress={() => setModalVisible(false)}>
+                                <AntDesign name="right" size={24} color="black" />
                             </TouchableOpacity>
                         </View>
-                    {mostrarDetalhes && (
-                        <View style={stylesConsulta.detalhesConsulta}>
-                            <Text style={stylesConsulta.detalheText}>Data: {consulta.data}</Text>
-                            <Text style={stylesConsulta.detalheText}>Horário: {consulta.horario}</Text>
-                            <View>
-                                <TouchableOpacity onPress={() => handleExcluirConsulta(index)}>
-                                    <Image style={stylesConsulta.LixoImg} source={require('../../../../assets/excluir.png')}/>
+                    </View>
+                </Modal>
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={excluirModalVisible}
+                    onRequestClose={() => {
+                        setExcluirModalVisible(false);
+                    }}
+                >
+                    <View style={stylesConsulta.modalBackground}>
+                        <View style={stylesConsulta.modalContent}>
+                            <Text>Deseja realmente excluir esta consulta?</Text>
+                            <View style={stylesConsulta.modalButtons}>
+                                <TouchableOpacity style={stylesConsulta.modalButton} onPress={cancelarExclusaoConsulta}>
+                                    <Text>Não</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={stylesConsulta.modalButton} onPress={confirmarExclusaoConsulta}>
+                                    <Text>Sim</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
-                    )}
                     </View>
-                
-                ))}
+                </Modal>
             </View>
-
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                    setModalVisible(!modalVisible);
-                }}
-            >
-                <View style={stylesConsulta.modalBackground}>
-                    <View style={stylesConsulta.modalContent}>
-                        <Text style={stylesConsulta.label}>Nome da Consulta</Text>
-                        <TextInput
-                            style={stylesConsulta.input}
-                            placeholder="Digite o nome da consulta"
-                            onChangeText={(text) => setConsultaInfo({ ...consultaInfo, nome: text })}
-                        />
-                        <Text style={stylesConsulta.label}>Data</Text>
-                        <TextInput
-                            style={stylesConsulta.input}
-                            placeholder="Digite a data da consulta"
-                            onChangeText={(text) => setConsultaInfo({ ...consultaInfo, data: text })}
-                        />
-                        <Text style={stylesConsulta.label}>Horário</Text>
-                        <TextInput
-                            style={stylesConsulta.input}
-                            placeholder="Digite o horário da consulta"
-                            onChangeText={(text) => setConsultaInfo({ ...consultaInfo, horario: text })}
-                        />
-                        <TouchableOpacity onPress={handleSalvarConsulta} style={[stylesConsulta.botaoSalvar, { backgroundColor: '#A1D5B0' }]}>
-                            <Text style={{ color: 'white', textAlign: 'center', fontSize: 16, }}>Salvar Consulta</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={stylesConsulta.arrowButton} onPress={() => setModalVisible(false)}>
-                            <AntDesign name="right" size={24} color="black" />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Modal>
-
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={excluirModalVisible}
-                onRequestClose={() => {
-                    setExcluirModalVisible(false);
-                }}
-            >
-                <View style={stylesConsulta.modalBackground}>
-                    <View style={stylesConsulta.modalContent}>
-                        <Text>Deseja realmente excluir esta consulta?</Text>
-                        <View style={stylesConsulta.modalButtons}>
-                            <TouchableOpacity style={stylesConsulta.modalButton} onPress={cancelarExclusaoConsulta}>
-                                <Text>Não</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={stylesConsulta.modalButton} onPress={confirmarExclusaoConsulta}>
-                                <Text>Sim</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-            </Modal>
-        </View>
+        </ThemeProvider>
     );
 }
